b297e4c4273d110ba3266961a568fe3fab0d97b6
dvlpr-shark <jihzang@naver.com>
2020-05-25 23:02:41 +0900

    프로젝트 생성 + hello world + unit test

---
+++ contact/src/test/java/com/zziri/contact/controller/HelloWorldControllerTest.java
@@ -0,0 +1,37 @@
+package com.zziri.contact.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld() {
+        assertThat(helloWorldController.helloWorld()).isEqualTo("HelloWorld");
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/helloWorld"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("HelloWorld"));
+    }
+
+}
