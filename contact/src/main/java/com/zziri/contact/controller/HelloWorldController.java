7da94b259737107cea65a6d03f42e767bf0fac4c
zziri <jihzang@naver.com>
2020-06-03 09:34:44 +0900

    주석 추가

--- contact/src/main/java/com/zziri/contact/controller/HelloWorldController.java
+++ contact/src/main/java/com/zziri/contact/controller/HelloWorldController.java
@@ -3,7 +3,7 @@ package com.zziri.contact.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController                                 // REST API를 정의할 수 있도록 해주는 어노테이션
 public class HelloWorldController {
     @GetMapping(value = "/api/helloWorld")
     public String helloWorld() {
