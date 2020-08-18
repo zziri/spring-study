00d6f0ab92028b39e7deb79fb3d53a1f4102008d
zziri <jihzang@naver.com>
2020-08-19 00:03:22 +0900

    PersonController: post 할때 DTO로 받아서 처리하도록 변경 PersonControllerTest: post test code refactoring

--- contact/src/test/java/com/zziri/contact/controller/PersonControllerTest.java
+++ contact/src/test/java/com/zziri/contact/controller/PersonControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
@@ -67,16 +68,23 @@ class PersonControllerTest {
 
     @Test
     void postPerson() throws Exception {
+        PersonDto dto = PersonDto.of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/api/person")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\n" +
-                                "  \"name\": \"martin2\",\n" +
-                                "  \"age\": 20,\n" +
-                                "  \"bloodType\": \"A\"\n" +
-                                "}"))
+                        .content(toJsonString(dto)))
                 .andDo(print())
                 .andExpect(status().isCreated());
+
+        Person result = personRepository.findAll(Sort.by(Sort.Direction.DESC, "id")).get(0);
+        assertAll(
+                ()->assertThat(result.getName()).isEqualTo("martin"),
+                ()->assertThat(result.getHobby()).isEqualTo("programming"),
+                ()->assertThat(result.getAddress()).isEqualTo("판교"),
+                ()->assertThat(result.getBirthday()).isEqualTo(Birthday.of(LocalDate.now())),
+                ()->assertThat(result.getJob()).isEqualTo("programmer"),
+                ()->assertThat(result.getPhoneNumber()).isEqualTo("010-1111-2222")
+        );
     }
 
     @Test
