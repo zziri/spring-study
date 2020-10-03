77a7badffe14e987b4afe768802f595475a4c846
zziri <jihzang@naver.com>
2020-10-03 15:58:33 +0900

    PersonServiceTest.java: test code refactoring(Mock)

--- contact/src/test/java/com/zziri/contact/service/PersonServiceTest.java
+++ contact/src/test/java/com/zziri/contact/service/PersonServiceTest.java
@@ -1,30 +1,36 @@
 package com.zziri.contact.service;
 
 import com.zziri.contact.domain.Person;
+import com.zziri.contact.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-    @Autowired
+    @InjectMocks
     private PersonService personService;
+    @Mock
+    private PersonRepository personRepository;
 
     @Test
     void getPeopleByName() {
+        when(personRepository.findByName("martin"))                             // when은 if와 비슷한 의미로 이해하자
+                .thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
-    }
-
-    @Test
-    void getPerson() {
-        Person person = personService.getPerson(3L);
-        assertThat(person.getName()).isEqualTo("dennis");
     }
 }
