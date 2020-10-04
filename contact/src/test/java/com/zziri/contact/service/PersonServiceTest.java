3ff39cb1031fcb5669118ed7eb3307afc3662442
zziri <jihzang@naver.com>
2020-10-04 23:58:28 +0900

    PersonServiceTest.java: 사용하지 않는 import 제거

--- contact/src/test/java/com/zziri/contact/service/PersonServiceTest.java
+++ contact/src/test/java/com/zziri/contact/service/PersonServiceTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
