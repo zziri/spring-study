4b7575df79474054cef9388d68d4105050aa7998
zziri <jihzang@naver.com>
2021-01-22 01:48:14 +0900

    springbasic: setup

---
+++ springbasic/src/main/java/com/zziri/study/Main.java
@@ -0,0 +1,11 @@
+package com.zziri.study;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!!");
+    }
+}
