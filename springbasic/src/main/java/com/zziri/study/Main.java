564c3c96e27d9365bfb0414c1604594046c96448
zziri <jihzang@naver.com>
2021-01-24 23:19:45 +0900

    jdbc run code, short log format

--- springbasic/src/main/java/com/zziri/study/Main.java
+++ springbasic/src/main/java/com/zziri/study/Main.java
@@ -2,10 +2,46 @@ package com.zziri.study;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+
 class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         logger.info("Hello world!!");
+
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("org.h2.Driver");
+            String url = "jdbc:h2:~/test; MODE=MySQL;";
+            connection = DriverManager.getConnection(url, "sa", "");
+            statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select id, username, password from member");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                logger.info(String.format("id: %d, username : %s, password : %s", id, username, password));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }
