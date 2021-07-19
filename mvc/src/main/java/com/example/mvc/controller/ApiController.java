a5c4728dabbc47dfee976c7d77ad90ded9dd126c
zziri <jihzang@naver.com>
2021-07-19 10:10:56 +0900

    intro: get api lecture

--- mvc/src/main/java/com/example/mvc/controller/ApiController.java
+++ mvc/src/main/java/com/example/mvc/controller/ApiController.java
@@ -2,9 +2,9 @@ package com.example.mvc.controller;
 
 
 import com.example.mvc.dto.UserReq;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -12,5 +12,28 @@ public class ApiController {
     @GetMapping(path = "/hello")
     public String hello() {
         return "hello spring boot!";
+    }
+
+    @GetMapping(path = "/get/hello")
+    public String getHello() {
+        return "get Hello";
+    }
+
+    @RequestMapping(path = "/get/hi", method = RequestMethod.GET)
+    public String hi() {
+        return "hi";
+    }
+
+    @GetMapping(path = "/get/path-variable/{name}")
+    public String pathVariable(@PathVariable("name") String name) {
+        System.out.println("PathVariable : " + name);
+        return name;
+    }
+
+    @GetMapping(path = "/get/query-param")
+    public String queryParam(@RequestParam Map<String, String> param) {
+        StringBuilder builder = new StringBuilder();
+        param.forEach((key, value) -> builder.append(String.format("%s = %s\n", key, value)));
+        return builder.toString();
     }
 }
