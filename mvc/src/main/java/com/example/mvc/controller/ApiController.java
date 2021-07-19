c82a0c7541a9d6c078918dea008816bb7017a536
zziri <jihzang@naver.com>
2021-07-19 09:50:31 +0900

    intro: hello world

--- mvc/src/main/java/com/example/mvc/controller/ApiController.java
+++ mvc/src/main/java/com/example/mvc/controller/ApiController.java
@@ -2,7 +2,6 @@ package com.example.mvc.controller;
 
 
 import com.example.mvc.dto.UserReq;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api")
 public class ApiController {
-    @GetMapping
-    public UserReq user(UserReq req) {
-        return req;
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "hello spring boot!";
     }
 }
